449772e2c94607945919020628f5238b7d825053
makeztc <makeztc@gmail.com>
2017-12-24 13:04:42 +0800

    finish lab3

--- lab3/process.c
+++ lab3/process.c
@@ -2,13 +2,41 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/times.h>
+#include <sys/types.h>
 
 #define HZ	100
 
 void cpuio_bound(int last, int cpu_time, int io_time);
-
+/*
+1.  所有子进程都并行运行,每个子进程的实际运行时间一般不超过30秒;
+2.  父进程向标准输出打印所有子进程的id,并在所有子进程都退出后才退出;
+*/
 int main(int argc, char * argv[])
 {
+	pid_t n_proc[10]; /*10个子进程 PID*/
+	int i;
+	for(i=0;i<10;i++)
+	{
+		n_proc[i] = fork();
+		/*子进程*/
+		if(n_proc[i] == 0)
+		{
+			cpuio_bound(20,2*i,20-2*i); /*每个子进程都占用20s*/
+			return 0; /*执行完cpuio_bound 以后，结束该子进程*/
+		}
+		/*fork 失败*/
+		else if(n_proc[i] < 0 )
+		{
+			printf("Failed to fork child process %d!\n",i+1);
+			return -1;
+		}
+		/*父进程继续fork*/
+	}
+	/*打印所有子进程PID*/
+	for(i=0;i<10;i++)
+		printf("Child PID: %d\n",n_proc[i]);
+	/*等待所有子进程完成*/
+	wait(&i);  /*Linux 0.11 上 gcc要求必须有一个参数, gcc3.4+则不需要*/ 
 	return 0;
 }
 
